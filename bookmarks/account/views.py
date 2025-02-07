a069f1ed7ec99cc40d0ca4b64c51fe7a97f68a6e
krzysztof kudra <krzysztof.kudra@gmail.com>
2025-02-08 00:16:40 +0100

    logowanie

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd= form.cleaned_data
+            user= authenticate(username= cd['username'],
+                               password= cd['password'])
+            
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnianie zakończyło się sukcesem')
+                else:
+                    return HttpResponse('Użytkownik jest zablokowany')
+            else:
+                return HttpResponse('Nieprawidłowe dane.')
+    
+    else:
+        form= LoginForm()
+
+    return render(request, 'account/login.html',{'form': form})
